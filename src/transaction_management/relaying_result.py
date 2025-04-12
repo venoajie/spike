f0dc5ab3f5ba250833de858029780c7829584ba6
venoajie <59521823+venoajie@users.noreply.github.com>
2025-04-13 04:05:54 +0700

    Update relaying_result.py

--- src/transaction_management/relaying_result.py
+++ src/transaction_management/relaying_result.py
@@ -170,12 +170,10 @@ async def sending_telegram(
     
 async def fetch_ohlcv(exchange, symbol, timeframe, limit):
     since = None
-    while True:
-        try:
-            ohlcv = await exchange.fetch_ohlcv(symbol, timeframe, since, limit)
-            if len(ohlcv):
-                first_candle = ohlcv[0]
-                datetime = exchange.iso8601(first_candle[0])
-                print(datetime, exchange.id, symbol, first_candle[1:])
-        except Exception as e:
-            print(type(e).__name__, str(e))
+    
+    ohlcv = await exchange.fetch_ohlcv(symbol, timeframe, since, limit)
+    if len(ohlcv):
+        first_candle = ohlcv[0]
+        datetime = exchange.iso8601(first_candle[0])
+        print(datetime, exchange.id, symbol, first_candle[1:])
+        log.warning(ohlcv)
