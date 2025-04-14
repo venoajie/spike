734193e7bdeba5fbe6768dacda913c30d40496ec
venoajie <59521823+venoajie@users.noreply.github.com>
2025-04-14 15:32:30 +0700

    Update relaying_result.py

--- src/transaction_management/relaying_result.py
+++ src/transaction_management/relaying_result.py
@@ -395,6 +395,7 @@ def updating_cache(
         
         result_summary.update({"symbol":symbol})
         result_summary.update({"timestamp":current_timestamp})
+        log.debug (f"result_summary {result_summary}")   
         
         result += result + [result_summary]
         
